d1bd92251f013422bf926bf972fb86dd9e295d5e
Joshua Whitfield <joshuawhitfield022@gmail.com>
2023-11-06 20:55:55 -0700

    Update operations.cjs

--- classes/operations.cjs
+++ classes/operations.cjs
@@ -2,6 +2,13 @@ const oper = {
     randomIdx: (max = 52) => {
         return Math.floor((Math.random() * max) + 0);
     },
+    findSubArray: (mainArray, subArray) => {
+        const mainStr = mainArray.join(',');
+        const subStr = subArray.join(',');
+        const index = mainStr.indexOf(subStr);
+      
+        return index !== -1 ? true : false;
+    },
 };
 
 module.exports = oper;
